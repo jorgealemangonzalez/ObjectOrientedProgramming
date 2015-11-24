439397aad2eecfe08f90dbc0f173236b589736c4
guinii <guinartarnau@gmail.com>
2015-11-24 17:52:01 +0100

    arnau

--- src/bookstore/Stock.java
+++ src/bookstore/Stock.java
@@ -5,10 +5,46 @@
  */
 package bookstore;
 
+import java.util.Currency;
+
 /**
  *
  * @author jorge aleman
  */
 public class Stock {
+    private Book book;
+    private int copies;
+    private double price;
+    private Currency currency;
     
+    public void Stock( Book bk, int cop , double pric, Currency curr ){
+        book = bk;
+        copies = cop;
+        price = pric;
+        currency = curr;
+    }
+    
+    public Book getBook(){
+        return book;
+    }
+    
+    public String getBooktitle(){
+        return book.getTitle();
+    }
+    
+    public int numberOfCopies(){
+        return copies;
+    }
+    
+    public void addCopies( int num ){
+        copies += num;
+    }
+    
+    public void removeCopies( int num ){
+        copies -= num;
+    }
+    
+    public double totalPrice(){
+        return price;
+    }
 }
