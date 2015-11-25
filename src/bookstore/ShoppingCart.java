f9e03fc649d59715ed6f7b113f531d222ef9cf9f
guinii <guinartarnau@gmail.com>
2015-11-25 17:50:38 +0100

    arnau

--- src/bookstore/ShoppingCart.java
+++ src/bookstore/ShoppingCart.java
@@ -5,21 +5,50 @@
  */
 package bookstore;
 
+import java.util.Currency;
+
 /**
  *
  * @author guini
  */
 public class ShoppingCart extends BookCollection{
     
-    private Catalog catalog;
+    protected Catalog catalog;
     
     public ShoppingCart( Catalog catinit ){
         catalog = catinit;
+        String s[] = catalog.booktitles();
+        for( int i = 0 ; i < s.length ; ++i){
+            StockInterface stock = catalog.getStock(s[i]); //hemos puesto publica el getStock para poder coger todos los Stocks 
+            stock.removeCopies(stock.numberOfCopies());
+            collection.add(stock);
+        }
     }
     
     public void addCopies( int num , String booktitl){
-        for(int i = 0 ; i <  catalog.size() ; ++i){
-            
+        super.addCopies(num, booktitl);
+        catalog.removeCopies(num, booktitl);
+    }
+    
+    public void removeCopies( int num , String booktitl){
+        super.removeCopies(num, booktitl);
+        catalog.addCopies(num, booktitl);
+    }
+    
+    public double totalPrice(){
+       double suma = 0;
+       for ( StockInterface stock : collection ) {
+          suma += stock.totalPrice();
+        }
+       return suma;
+    }
+    
+    public void checkout () {
+     
+        Payment user = new Payment ();
+        user.doPayment(122541, "Arnau ", totalPrice(), Currency.getInstance("EUR"));
+        for( StockInterface stock : collection){
+            stock.removeCopies(stock.numberOfCopies());
         }
     }
 }
